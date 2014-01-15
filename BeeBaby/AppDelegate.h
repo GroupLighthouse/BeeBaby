28513f7305e4316d3658d8b5f190bb365ddf05d9
Marlon Santos Constante <marlon.constante@gmail.com>
2014-01-15 17:00:17 -0200

    Criando projeto

---
+++ BeeBaby/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeeBaby
+//
+//  Created by Marlon Santos Constante on 15/01/14.
+//  Copyright (c) 2014 Lighthouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeeBaby/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BeeBaby
+//
+//  Created by Marlon Santos Constante on 15/01/14.
+//  Copyright (c) 2014 Lighthouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
